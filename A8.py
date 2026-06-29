43691f2612956839d242f805e897269fa555bf42
Hari Jadhav <hrjadhav831@gmail.com>
2026-06-29 11:00:18 +0530

    Create Employee class using dictionary

---
+++ A8.py
@@ -0,0 +1,33 @@
+class Employee:
+
+    def __init__(self, emp_id, name, department, salary):
+        self.emp_id = emp_id
+        self.name = name
+        self.details = (department, salary)
+
+    def show_details(self):
+        print("Employee ID :", self.emp_id)
+        print("Name :", self.name)
+        print("Department :", self.details[0])
+        print("Salary :", self.details[1])
+        print("-------------------------")
+
+
+employees = {}
+
+for i in range(3):
+    print(f"\nEnter Details of Employee {i+1}")
+
+    emp_id = input("Employee ID : ")
+    name = input("Name : ")
+    department = input("Department : ")
+    salary = float(input("Salary : "))
+
+    emp = Employee(emp_id, name, department, salary)
+
+    employees[emp_id] = emp
+
+print("\nEmployee Details")
+
+for emp in employees.values():
+    emp.show_details()
